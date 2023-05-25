32619688c2a419132cd765916127f47542a28fc9
KarenDay <daianastar1@gmail.com>
2023-05-25 13:03:06 -0300

    primer commit

---
+++ TP2-Grupo99/src/main/java/ar/edu/unju/fi/Tp2Grupo99Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2Grupo99Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp2Grupo99Application.class, args);
+	}
+
+}
